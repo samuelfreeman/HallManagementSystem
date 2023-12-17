20aea67dde187c46fb15d51795e61acecc2b3418
Clement Obeng <oklement3@gmail.com>
2023-12-17 22:59:23 +0000

    update
    change made for occupants

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
-const PORT = 7070;
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+
+
 const bodyparser = require("body-parser");
 const routerBlock = require("./Route/blocks");
 //middleware for body parser
