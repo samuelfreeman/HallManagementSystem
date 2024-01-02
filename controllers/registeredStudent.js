972cdbc50408cd6651c7d1cb6e720a303e21d51b
lockwud <oklement3@gmail.com>
2024-01-02 15:31:33 +0000

    updated chema

--- controllers/registeredStudent.js
+++ controllers/registeredStudent.js
@@ -1,3 +1,26 @@
-// const { PrismaClient } = require('@prisma/client');
+///registering a new student in the hall assuming the have paid their fees 
 
-// const prisma = new PrismaClient();
+
+const { PrismaClient } = require('@prisma/client');
+
+
+const prisma = new PrismaClient();
+const student = async(req, res, next) =>{
+    try{
+        const data = req.body;
+        const student = await prisma.student.create({
+            data,
+        });
+        res.status(201).json({
+            student,
+        });
+        next()
+    }catch(e){
+        console.log(e)
+    }
+    
+}
+
+
+
+module.exports = student;
--- server.js
+++ server.js
@@ -1,21 +1,23 @@
+/// Importing express package and other middlewares
 const express = require('express');
-
+const dotenv = require('dotenv')
+dotenv.config()
 const PORT = process.env.PORT || 8080;
-const app = express();
+
+const app = express(); /// Involking express to the variable app to speed up out serve
 
 const helmet = require('helmet');
 const cors = require('cors');
 const compression = require('compression');
 const bodyparser = require('body-parser');
-require('dotenv').config();
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(helmet());
 app.use(compression());
-const routerBlock = require('./Route/blocks');
+const routerBlock = require('./routes/blocks');
 // middleware for body parser
 app.use(bodyparser.json());
 app.use('/api', routerBlock);
 app.listen(PORT, () => {
-  console.log('Server is running!');
+  console.log('Server running on port', `${PORT}`);
 });
