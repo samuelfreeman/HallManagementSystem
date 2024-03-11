6cc101cd754bcee3d4d12df2b91cc0593c98c61d
samuel freeman <samuelmanueljnr@gmail.com>
2024-03-11 21:09:30 +0000

    added controller to  create a request for a room  and  to get all incoming requests

--- src/controllers/roomRequest.js
+++ src/controllers/roomRequest.js
@@ -1,14 +1,36 @@
+const e = require("express");
 const logger = require("../utils/loggerUtil");
 const prisma = require("../utils/prismaUtil");
 
+exports.requestRoom = async (req, res, next) => {
+  try {
+    const data = req.body;
+    const roomRequest = await prisma.roomRequest.create({
+      data,
+    });
+    res.status(200).json({
+      roomRequest,
+    });
+  } catch (error) {
+    logger.error(error);
+    next(error);
+  }
+};
 
+exports.getRequests = async (req, res, next) => {
+  try {
+    const requests = await prisma.roomRequest.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    res.status(200).json({
+      requests,
+    });
+  } catch (error) {
+    logger.error(error);
+    next(error);
+  }
+};
 
-
-exports.requestRoom = async(req,res,next) =>{
-try {
-    const roomRequest = await prisma.re
-} catch (error) {
-    
-}
-}
 
