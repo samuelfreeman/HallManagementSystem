b6c2680333e772fc0ba5e2a0945d9e9bec732cf9
samuel freeman <samuelmanueljnr@gmail.com>
2024-03-11 19:31:17 +0000

    worked on the room request

---
+++ src/controllers/roomRequest.js
@@ -0,0 +1,14 @@
+const logger = require("../utils/loggerUtil");
+const prisma = require("../utils/prismaUtil");
+
+
+
+
+exports.requestRoom = async(req,res,next) =>{
+try {
+    const roomRequest = await prisma.re
+} catch (error) {
+    
+}
+}
+
