c3046bfae7348787c38aa0b0f08ef293f1f3d1ed
samuel freeman <samuelmanueljnr@gmail.com>
2024-03-13 15:33:05 +0000

    checking husky

--- src/routes/v1/mobile/rooms.js
+++ src/routes/v1/mobile/rooms.js
@@ -5,7 +5,12 @@ const router = express.Router();
 const room = require("../../../controllers/room");
 const requestRoom = require("../../../controllers/roomRequest");
 
+
 router.post("/request-room", requestRoom.requestRoom);
+
 router.get("/available", room.getAvailableRooms);
+
 router.get("/:studentId", requestRoom.getRequest_by_studentId);
+
 module.exports = router;
+
--- src/routes/v1/web/index.js
+++ src/routes/v1/web/index.js
@@ -14,6 +14,8 @@ const allocation = require("./allocation");
 const department = require("./department");
 
 const room = require("./rooms");
+
+const requests = require("./requests");
 /// using our route middleware
 
 route.use("/admin", adminRoute);
@@ -27,5 +29,7 @@ route.use("/allocation", allocation);
 route.use("/department", department);
 
 route.use("/room", room);
+
+route.use("/request",requests)
 
 module.exports = route;
