24e7008cb2e8b85c380021a91a37d31a3a8b7e3c
samuel freeman <samuelmanueljnr@gmail.com>
2024-03-13 16:48:59 +0000

    did some cleaning up

--- src/routes/v1/web/index.js
+++ src/routes/v1/web/index.js
@@ -15,9 +15,9 @@ const department = require('./department')
 
 const room = require('./rooms')
 
-const room = require("./rooms");
+const room = require('./rooms')
 
-const requests = require("./requests");
+const requests = require('./requests')
 /// using our route middleware
 
 route.use('/admin', adminRoute)
@@ -32,8 +32,6 @@ route.use('/department', department)
 
 route.use('/room', room)
 
-route.use("/room", room);
-
-route.use("/request",requests)
+route.use('/request', requests)
 
 module.exports = route
