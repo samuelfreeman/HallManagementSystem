45192b90e5a410c9017cb5810046cd219416ff4d
lockwud <oklement3@gmail.com>
2024-03-13 00:59:51 +0000

    update

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 /// Importing express package and other middlewares
 const express = require('express')
 
-const app = express() /// Involking express to the variable app to speed up out serve
-const dotenv = require('dotenv')
+const app = express(); /// Involking express to the variable app to speed up out serve
+const dotenv = require("dotenv");
+const morgan = require("morgan");
 
 dotenv.config()
 const PORT = process.env.PORT || 8080
